791b4ab6c79bb45b7732658fefa3881f25b03a14
Erik Simmler <tgecho@gmail.com>
2016-03-13 21:06:49 -0400

    Exit build.sh with an error if one of the exercise examples fails the test run

--- bin/build.sh
+++ bin/build.sh
@@ -7,6 +7,12 @@ do
   echo '-------------------------------------------------------'
   echo "Testing $exercise"
   elm-test $exercise_dir/*Tests.elm
+  TEST_RESULT=$?
   mv "$exercise_dir/$exercise.elm" "$exercise_dir/$exercise.example"
   mv "$exercise_dir/$exercise.impl" "$exercise_dir/$exercise.elm"
+
+  if [ $TEST_RESULT -ne 0 ]; then
+    echo "$exercise failed";
+    exit $TEST_RESULT;
+  fi
 done
