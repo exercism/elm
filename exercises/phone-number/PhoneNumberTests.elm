90e7d8837461d9bb424114907d4d2a8ab110e5ae
Erik Simmler <tgecho@gmail.com>
2016-03-16 21:33:59 -0400

    Revise phone-number to use Maybe

--- exercises/phone-number/PhoneNumberTests.elm
+++ exercises/phone-number/PhoneNumberTests.elm
@@ -3,7 +3,7 @@ module Main (..) where
 import Task
 import Console
 import ElmTest exposing (..)
-import PhoneNumber exposing (getNumber, printPretty)
+import PhoneNumber exposing (getNumber, prettyPrint)
 
 
 tests : Test
@@ -12,40 +12,40 @@ tests =
     "PhoneNumber"
     [ test
         "cleans number"
-        (assertEqual "1234567890" (getNumber "(123) 456-7890"))
+        (assertEqual (Just "1234567890") (getNumber "(123) 456-7890"))
     , test
         "cleans number with dots"
-        (assertEqual "1234567890" (getNumber "123.456.7890"))
+        (assertEqual (Just "1234567890") (getNumber "123.456.7890"))
     , test
         "valid when 11 digits and first is 1"
-        (assertEqual "1234567890" (getNumber "11234567890"))
+        (assertEqual (Just "1234567890") (getNumber "11234567890"))
     , test
         "invalid when 11 digits"
-        (assertEqual "0000000000" (getNumber "21234567890"))
+        (assertEqual Nothing (getNumber "21234567890"))
     , test
         "invalid when 9 digits"
-        (assertEqual "0000000000" (getNumber "123456789"))
+        (assertEqual Nothing (getNumber "123456789"))
     , test
         "invalid when 12 digits"
-        (assertEqual "0000000000" (getNumber "123456789012"))
+        (assertEqual Nothing (getNumber "123456789012"))
     , test
         "invalid when empty"
-        (assertEqual "0000000000" (getNumber ""))
+        (assertEqual Nothing (getNumber ""))
     , test
         "invalid when no digits present"
-        (assertEqual "0000000000" (getNumber " (-) "))
+        (assertEqual Nothing (getNumber " (-) "))
     , test
         "valid with leading characters"
-        (assertEqual "1234567890" (getNumber "my number is 123 456 7890"))
+        (assertEqual (Just "1234567890") (getNumber "my number is 123 456 7890"))
     , test
         "valid with trailing characters"
-        (assertEqual "1234567890" (getNumber "123 456 7890 - bob"))
+        (assertEqual (Just "1234567890") (getNumber "123 456 7890 - bob"))
     , test
         "pretty print"
-        (assertEqual "(123) 456-7890" (printPretty "1234567890"))
+        (assertEqual (Just "(123) 456-7890") (prettyPrint "1234567890"))
     , test
         "pretty print with full us phone number"
-        (assertEqual "(123) 456-7890" (printPretty "11234567890"))
+        (assertEqual (Just "(123) 456-7890") (prettyPrint "11234567890"))
     ]
 
 
