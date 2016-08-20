4085701330d6496dfe07358b982c8af1ca57b27d
Erik Simmler <tgecho@gmail.com>
2016-08-20 16:14:27 -0400

    Add the runtests files back with a message

---
+++ exercises/raindrops/runtests.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+echo "We've changed how tests are run! Please review the latest install/running docs at http://exercism.io/languages/elm and report any issues at https://github.com/exercism/xelm"
