bb4b15ce0ca82dc0206cb3788c4243263e7cbc6a
Joshua Stoutenburg <jehoshua02@gmail.com>
2016-08-28 22:01:33 -0600

    Refactor Gigasecond.add to Speak Date Instead of String

--- exercises/gigasecond/GigasecondTests.elm
+++ exercises/gigasecond/GigasecondTests.elm
@@ -4,6 +4,7 @@ import Test.Runner.Node exposing (run)
 import Json.Encode exposing (Value)
 import Test exposing (..)
 import Expect
+import Date
 import Gigasecond exposing (add)
 
 
@@ -13,21 +14,31 @@ tests =
         [ describe "add"
             [ test "2011-04-25" <|
                 \() ->
-                    Expect.equal (Ok "2043-01-01T01:46:40") (Gigasecond.add "2011-04-25")
+                    Expect.equal (actual "2011-04-25") (expected "2043-01-01T01:46:40")
             , test "1977-06-13" <|
                 \() ->
-                    Expect.equal (Ok "2009-02-19T01:46:40") (Gigasecond.add "1977-06-13")
+                    Expect.equal (actual "1977-06-13") (expected "2009-02-19T01:46:40")
             , test "1959-07-19" <|
                 \() ->
-                    Expect.equal (Ok "1991-03-27T01:46:40") (Gigasecond.add "1959-07-19")
+                    Expect.equal (actual "1959-07-19") (expected "1991-03-27T01:46:40")
             , test "full time specified" <|
                 \() ->
-                    Expect.equal (Ok "2046-10-02T23:46:40") (Gigasecond.add "2015-01-24T22:00:00")
+                    Expect.equal (actual "2015-01-24T22:00:00") (expected "2046-10-02T23:46:40")
             , test "full time with day roll-over" <|
                 \() ->
-                    Expect.equal (Ok "2046-10-03T01:46:39") (Gigasecond.add "2015-01-24T23:59:59")
+                    Expect.equal (actual "2015-01-24T23:59:59") (expected "2046-10-03T01:46:39")
             ]
         ]
+
+
+actual : String -> Result String Date.Date
+actual =
+    Date.fromString >> Result.map Gigasecond.add
+
+
+expected : String -> Result String Date.Date
+expected =
+    Date.fromString
 
 
 main : Program Value
