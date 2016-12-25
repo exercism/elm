b98c4dbacb6d2d886aaf4bc50f7139c6ae83e596
Erik Schierboom <erik_schierboom@hotmail.com>
2016-12-25 10:57:15 +0100

    Add grains exercise

---
+++ exercises/grains/Grains.elm
@@ -0,0 +1,2 @@
+module Grains exposing (..)
+
---
+++ exercises/grains/Grains.example.elm
@@ -0,0 +1,9 @@
+module Grains exposing (..)
+
+
+square : Int -> Maybe Int
+square n =
+    if n < 1 then
+        Nothing
+    else
+        Just <| 2 ^ (n - 1)
