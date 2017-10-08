8bc34db0f212156328c6f63d7d4769be0f67de9e
Morten Nygaard Åsnes <mna@free2.no>
2017-10-08 17:57:29 +0200

    Correct formatting

--- exercises/collatz-conjecture/tests/Tests.elm
+++ exercises/collatz-conjecture/tests/Tests.elm
@@ -15,25 +15,24 @@ tests =
         -- Once you get the first test passing, remove the
         -- `skip <|` (just leave the comma) on the next
         -- lines to continue!
-
         , skip <|
             test "divide if even" <|
-            \() ->
-                Expect.equal (Ok 4) (collatz 16)
+                \() ->
+                    Expect.equal (Ok 4) (collatz 16)
         , skip <|
             test "even and odd step" <|
-            \() ->
-                Expect.equal (Ok 9) (collatz 12)
+                \() ->
+                    Expect.equal (Ok 9) (collatz 12)
         , skip <|
             test "Large number of even and odd step" <|
-            \() ->
-                Expect.equal (Ok 152) (collatz 1000000)
+                \() ->
+                    Expect.equal (Ok 152) (collatz 1000000)
         , skip <|
             test "zero is an error" <|
-            \() ->
-                Expect.equal (Err "Only positive numbers are allowed") (collatz 0)
+                \() ->
+                    Expect.equal (Err "Only positive numbers are allowed") (collatz 0)
         , skip <|
             test "negative values is an error" <|
-            \() ->
-                Expect.equal (Err "Only positive numbers are allowed") (collatz -15)
+                \() ->
+                    Expect.equal (Err "Only positive numbers are allowed") (collatz -15)
         ]
