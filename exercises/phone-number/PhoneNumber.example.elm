5c32ba3f2e199eca686a01579fd2f913d66c65a8
Phippsaurus <31800244+Phippsaurus@users.noreply.github.com>
2018-01-08 10:51:02 +0100

    Phone Number only valid if N != 1
    After suggested changes to [Phone-Number-Test](https://github.com/Phippsaurus/elm/blob/master/exercises/phone-number/tests/Tests.elm), the example implementation needs to be updated accordingly.
    
    The example maybe could be simpler but passes all tests now.

--- exercises/phone-number/PhoneNumber.example.elm
+++ exercises/phone-number/PhoneNumber.example.elm
@@ -1,44 +1,60 @@
 module PhoneNumber exposing (..)
 
-import String
+import Regex exposing (Regex, regex, find, HowMany(..))
+import String exposing (concat, startsWith, length, slice)
+import List exposing (map, head)
+import Maybe exposing (andThen)
 
 
-nums =
-    [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' ]
+matchesFormat : Regex -> String -> Bool
+matchesFormat regex string =
+    case find (AtMost 1) regex string |> head of
+        Just found ->
+            True
+
+        Nothing ->
+            False
 
 
 getNumber : String -> Maybe String
-getNumber text =
-    getValidNum (String.filter isDigit text)
+getNumber phoneNumber =
+    let
+        numberFormat =
+            regex "^\\D*(\\+?1[\\-\\s\\.]?)?((\\([2-9]\\d{2}\\))|([2-9]\\d{2}))[\\-\\s\\.]?[2-9]\\d{2}[\\-\\s\\.]?\\d{4}\\D*$"
 
+        numbers =
+            regex "\\d"
 
-isDigit : Char -> Bool
-isDigit char =
-    List.any (\n -> n == char) nums
+        dropCountryCode matchedNumbers =
+            case matchedNumbers of
+                "1" :: nums ->
+                    nums
 
+                _ ->
+                    matchedNumbers
 
-getValidNum : String -> Maybe String
-getValidNum num =
-    if String.length num == 10 then
-        Just num
-    else if (String.length num == 11) && (String.left 1 num == "1") then
-        Just (String.dropLeft 1 num)
-    else
-        Nothing
+        stripNumber =
+            concat << dropCountryCode << map .match << find All numbers
+    in
+        if phoneNumber |> matchesFormat numberFormat then
+            Just <| stripNumber <| phoneNumber
+        else
+            Nothing
 
 
 prettyPrint : String -> Maybe String
-prettyPrint input =
-    Maybe.map formatNumber (getNumber input)
+prettyPrint phoneNumber =
+    let
+        numberFormat =
+            regex "^1?[2-9]\\d{2}[2-9]\\d{2}\\d{4}$"
 
-
-formatNumber : String -> String
-formatNumber input =
-    String.concat
-        [ "("
-        , (String.slice 0 3 input)
-        , ") "
-        , (String.slice 3 6 input)
-        , "-"
-        , (String.slice 6 10 input)
-        ]
+        prettyNumber =
+            if phoneNumber |> startsWith "1" then
+                "(" ++ (slice 1 4 phoneNumber) ++ ") " ++ (slice 4 7 phoneNumber) ++ "-" ++ (slice 7 11 phoneNumber)
+            else
+                "(" ++ (slice 0 3 phoneNumber) ++ ") " ++ (slice 3 6 phoneNumber) ++ "-" ++ (slice 6 10 phoneNumber)
+    in
+        if phoneNumber |> matchesFormat numberFormat then
+            Just prettyNumber
+        else
+            Nothing
