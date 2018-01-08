920bca8652e4d71149a6f98a813c53f4ab7bce0b
Phippsaurus <31800244+Phippsaurus@users.noreply.github.com>
2018-01-08 10:32:13 +0100

    Update Tests.elm
    The format of _NANP_ phone numbers is described in the [README](https://github.com/exercism/elm/edit/master/exercises/phone-number/README.md) as 
    
    `````
    (NXX)-NXX-XXXX
    `````
    
    where `N` is a digit from 2 through 9, but all the tests for valid phone numbers test for `Just "1234567890"`.
    That means the first digit of the _area code_, `N`, would be 1 there every time, for which a correct implementation should return `Nothing`.
    
    My change would simply increment those 1s to 2, as i believe the intention is to test for valid numbers here.
    
    Additionally I introduced two new test cases, expecting `Nothing`, when either digit `N` in the format is equal to 1.

--- exercises/phone-number/tests/Tests.elm
+++ exercises/phone-number/tests/Tests.elm
@@ -9,13 +9,13 @@ tests : Test
 tests =
     describe "PhoneNumber"
         [ test "cleans number" <|
-            \() -> Expect.equal (Just "1234567890") (getNumber "(123) 456-7890")
+            \() -> Expect.equal (Just "2234567890") (getNumber "(223) 456-7890")
         , skip <|
             test "cleans number with dots" <|
-                \() -> Expect.equal (Just "1234567890") (getNumber "123.456.7890")
+                \() -> Expect.equal (Just "2234567890") (getNumber "223.456.7890")
         , skip <|
             test "valid when 11 digits and first is 1" <|
-                \() -> Expect.equal (Just "1234567890") (getNumber "11234567890")
+                \() -> Expect.equal (Just "2234567890") (getNumber "12234567890")
         , skip <|
             test "invalid when 11 digits" <|
                 \() -> Expect.equal Nothing (getNumber "21234567890")
@@ -26,6 +26,12 @@ tests =
             test "invalid when 12 digits" <|
                 \() -> Expect.equal Nothing (getNumber "123456789012")
         , skip <|
+            test "invalid when N in area code is 1" <|
+                \() -> Expect.equal Nothing (getNumber "1234567890")
+        , skip <|
+            test "invalid when N in exchange code is 1" <|
+                \() -> Expect.equal Nothing (getNumber "2341567890")
+        , skip <|
             test "invalid when empty" <|
                 \() -> Expect.equal Nothing (getNumber "")
         , skip <|
@@ -33,14 +39,14 @@ tests =
                 \() -> Expect.equal Nothing (getNumber " (-) ")
         , skip <|
             test "valid with leading characters" <|
-                \() -> Expect.equal (Just "1234567890") (getNumber "my number is 123 456 7890")
+                \() -> Expect.equal (Just "2234567890") (getNumber "my number is 223 456 7890")
         , skip <|
             test "valid with trailing characters" <|
-                \() -> Expect.equal (Just "1234567890") (getNumber "123 456 7890 - bob")
+                \() -> Expect.equal (Just "2234567890") (getNumber "223 456 7890 - bob")
         , skip <|
             test "pretty print" <|
-                \() -> Expect.equal (Just "(123) 456-7890") (prettyPrint "1234567890")
+                \() -> Expect.equal (Just "(223) 456-7890") (prettyPrint "2234567890")
         , skip <|
             test "pretty print with full us phone number" <|
-                \() -> Expect.equal (Just "(123) 456-7890") (prettyPrint "11234567890")
+                \() -> Expect.equal (Just "(223) 456-7890") (prettyPrint "12234567890")
         ]
