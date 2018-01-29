6c464f30012af5dee1fab785a7e32376f2bca077
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2018-01-29 12:27:30 +0000

    Return a Result instead of Maybe

--- exercises/hamming/Hamming.example.elm
+++ exercises/hamming/Hamming.example.elm
@@ -3,12 +3,12 @@ module Hamming exposing (..)
 import String exposing (length, toList)
 
 
-distance : String -> String -> Maybe Int
+distance : String -> String -> Result String Int
 distance left right =
     if length left /= length right then
-        Nothing
+        Err "left and right strands must be of equal length"
     else
         List.map2 (\l r -> l /= r) (toList left) (toList right)
             |> List.filter identity
             |> List.length
-            |> Just
+            |> Ok
--- exercises/hamming/tests/Tests.elm
+++ exercises/hamming/tests/Tests.elm
@@ -9,44 +9,44 @@ tests : Test
 tests =
     describe "Hamming"
         [ test "identical strands" <|
-            \() -> Expect.equal (Just 0) (distance "A" "A")
+            \() -> Expect.equal (Ok 0) (distance "A" "A")
         , skip <|
             test "long identical strands" <|
-                \() -> Expect.equal (Just 0) (distance "GGACTGA" "GGACTGA")
+                \() -> Expect.equal (Ok 0) (distance "GGACTGA" "GGACTGA")
         , skip <|
             test "complete distance in single nucleotide strands" <|
-                \() -> Expect.equal (Just 1) (distance "A" "G")
+                \() -> Expect.equal (Ok 1) (distance "A" "G")
         , skip <|
             test "complete distance in small strands" <|
-                \() -> Expect.equal (Just 2) (distance "AG" "CT")
+                \() -> Expect.equal (Ok 2) (distance "AG" "CT")
         , skip <|
             test "small distance in small strands" <|
-                \() -> Expect.equal (Just 1) (distance "AT" "CT")
+                \() -> Expect.equal (Ok 1) (distance "AT" "CT")
         , skip <|
             test "small distance" <|
-                \() -> Expect.equal (Just 1) (distance "GGACG" "GGTCG")
+                \() -> Expect.equal (Ok 1) (distance "GGACG" "GGTCG")
         , skip <|
             test "small distance in long strands" <|
-                \() -> Expect.equal (Just 2) (distance "ACCAGGG" "ACTATGG")
+                \() -> Expect.equal (Ok 2) (distance "ACCAGGG" "ACTATGG")
         , skip <|
             test "non-unique character in first strand" <|
-                \() -> Expect.equal (Just 1) (distance "AGA" "AGG")
+                \() -> Expect.equal (Ok 1) (distance "AGA" "AGG")
         , skip <|
             test "non-unique character in second strand" <|
-                \() -> Expect.equal (Just 1) (distance "AGG" "AGA")
+                \() -> Expect.equal (Ok 1) (distance "AGG" "AGA")
         , skip <|
             test "large distance" <|
-                \() -> Expect.equal (Just 4) (distance "GATACA" "GCATAA")
+                \() -> Expect.equal (Ok 4) (distance "GATACA" "GCATAA")
         , skip <|
             test "large distance in off-by-one strand" <|
-                \() -> Expect.equal (Just 9) (distance "GGACGGATTCTG" "AGGACGGATTCT")
+                \() -> Expect.equal (Ok 9) (distance "GGACGGATTCTG" "AGGACGGATTCT")
         , skip <|
             test "empty strands" <|
-                \() -> Expect.equal (Just 0) (distance "" "")
+                \() -> Expect.equal (Ok 0) (distance "" "")
         , skip <|
             test "disallow first strand longer" <|
-                \() -> Expect.equal Nothing (distance "AATG" "AAA")
+                \() -> Expect.equal (Err "left and right strands must be of equal length") (distance "AATG" "AAA")
         , skip <|
             test "disallow second strand longer" <|
-                \() -> Expect.equal Nothing (distance "ATA" "AGTG")
+                \() -> Expect.equal (Err "left and right strands must be of equal length") (distance "ATA" "AGTG")
         ]
