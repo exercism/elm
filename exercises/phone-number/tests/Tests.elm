5764af5871f0c99f814ca2337ae5493aa4241314
philipp_trucksaess <philipp.trucksaess@gmail.com>
2018-02-13 21:34:31 +0100

    Changed the order of tests, so that valid and invalid inputs are interspersed.

--- exercises/phone-number/tests/Tests.elm
+++ exercises/phone-number/tests/Tests.elm
@@ -11,26 +11,29 @@ tests =
         [ test "cleans number" <|
             \() -> Expect.equal (Just "2234567890") (getNumber "(223) 456-7890")
         , skip <|
+            test "invalid when empty" <|
+                \() -> Expect.equal Nothing (getNumber "")
+        , skip <|
             test "cleans number with dots" <|
                 \() -> Expect.equal (Just "2234567890") (getNumber "223.456.7890")
-        , skip <|
-            test "cleans number with multiple spaces" <|
-                \() -> Expect.equal (Just "2234567890") (getNumber "223 456   7890")
-        , skip <|
-            test "valid when 11 digits and first is 1" <|
-                \() -> Expect.equal (Just "2234567890") (getNumber "12234567890")
-        , skip <|
-            test "valid when 11 digits and first is 1 with punctuation" <|
-                \() -> Expect.equal (Just "2234567890") (getNumber "+1 (223) 456-7890")
-        , skip <|
-            test "invalid when 11 digits and first is not 1" <|
-                \() -> Expect.equal Nothing (getNumber "22234567890")
         , skip <|
             test "invalid when 9 digits" <|
                 \() -> Expect.equal Nothing (getNumber "223456789")
         , skip <|
+            test "cleans number with multiple spaces" <|
+                \() -> Expect.equal (Just "2234567890") (getNumber "223 456   7890")
+        , skip <|
             test "invalid when 12 digits" <|
                 \() -> Expect.equal Nothing (getNumber "223456789012")
+        , skip <|
+            test "valid when 11 digits and first is 1" <|
+                \() -> Expect.equal (Just "2234567890") (getNumber "12234567890")
+        , skip <|
+            test "invalid when 11 digits and first is not 1" <|
+                \() -> Expect.equal Nothing (getNumber "22234567890")
+        , skip <|
+            test "valid when 11 digits and first is 1 with punctuation" <|
+                \() -> Expect.equal (Just "2234567890") (getNumber "+1 (223) 456-7890")
         , skip <|
             test "invalid with letters" <|
                 \() -> Expect.equal Nothing (getNumber "223-abc-789012")
@@ -53,17 +56,8 @@ tests =
             test "invalid when N in exchange code is 1" <|
                 \() -> Expect.equal Nothing (getNumber "2341567890")
         , skip <|
-            test "invalid when empty" <|
-                \() -> Expect.equal Nothing (getNumber "")
-        , skip <|
             test "invalid when no digits present" <|
                 \() -> Expect.equal Nothing (getNumber " (-) ")
-        , skip <|
-            test "valid with leading characters" <|
-                \() -> Expect.equal (Just "2234567890") (getNumber "my number is 223 456 7890")
-        , skip <|
-            test "valid with trailing characters" <|
-                \() -> Expect.equal (Just "2234567890") (getNumber "223 456 7890 - bob")
         , skip <|
             test "pretty print" <|
                 \() -> Expect.equal (Just "(223) 456-7890") (prettyPrint "2234567890")
