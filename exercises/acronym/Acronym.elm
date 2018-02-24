c3399684cf713fa6477886c17815e65f8bdbf1e2
Kenneth Bogner <tuxagon91@gmail.com>
2018-02-23 22:45:25 -0500

    Added Acronym exercise

---
+++ exercises/acronym/Acronym.elm
@@ -0,0 +1,1 @@
+module Acronym exposing (..)
---
+++ exercises/acronym/tests/Tests.elm
@@ -0,0 +1,35 @@
+module Tests exposing (..)
+
+import Acronym exposing (abbreviate)
+import Expect
+import Test exposing (Test, describe, test, skip)
+
+
+tests : Test
+tests =
+    describe "Acronym"
+        [ test "basic" <|
+            \_ ->
+                Expect.equal "PNG"
+                    (abbreviate "Portable Network Graphics")
+        , skip <|
+            test "lowercase words" <|
+                \_ ->
+                    Expect.equal "ROR"
+                        (abbreviate "Ruby on Rails")
+        , skip <|
+            test "punctuation" <|
+                \_ ->
+                    Expect.equal "FIFO"
+                        (abbreviate "First In, First Out")
+        , skip <|
+            test "all caps word" <|
+                \_ ->
+                    Expect.equal "GIMP"
+                        (abbreviate "GNU Image Manipulation Program")
+        , skip <|
+            test "punctuation without whitespace" <|
+                \_ ->
+                    Expect.equal "CMOS"
+                        (abbreviate "Complementary metal-oxide semiconductor")
+        ]
