1ff4483aeb823e6f56658baa3c0e372161e32cf7
Marcy Rattner <mrattner42@outlook.com>
2018-05-09 18:19:44 +1200

    Rename tests to match canonical names; add missing canonical test cases

--- exercises/run-length-encoding/tests/Tests.elm
+++ exercises/run-length-encoding/tests/Tests.elm
@@ -1,11 +1,11 @@
 module Tests exposing (..)
 
+import Char
 import Expect
-import RunLengthEncoding exposing (decode, encode, version)
-import Test exposing (..)
 import Fuzz exposing (Fuzzer)
 import Regex exposing (regex)
-import Char
+import RunLengthEncoding exposing (decode, encode, version)
+import Test exposing (..)
 
 
 tests : Test
@@ -13,23 +13,47 @@ tests =
     describe "RunLengthEncoding"
         [ test "the solution is for the correct version of the test" <|
             \() -> Expect.equal 2 version
-        , test "encode simple" <|
-            \() -> Expect.equal "2A3B4C" (encode "AABBBCCCC")
+        , test "encode empty string" <|
+            \() -> Expect.equal "" (encode "")
         , skip <|
-            test "decode simple" <|
+            test "encode single characters only" <|
+                \() -> Expect.equal "XYZ" (encode "XYZ")
+        , skip <|
+            test "encode multiple whitespace mixed in string" <|
+                \() -> Expect.equal "2 hs2q q2w2 " (encode "  hsqq qww  ")
+        , skip <|
+            test "encode lowercase characters" <|
+                \() -> Expect.equal "2a3b4c" (encode "aabbbcccc")
+        , skip <|
+            test "decode empty string" <|
+                \() -> Expect.equal "" (decode "")
+        , skip <|
+            test "decode single characters only" <|
+                \() -> Expect.equal "XYZ" (decode "XYZ")
+        , skip <|
+            test "decode multiple whitespace mixed in string" <|
+                \() -> Expect.equal "  hsqq qww  " (decode "2 hs2q q2w2 ")
+        , skip <|
+            test "decode lower case string" <|
+                \() -> Expect.equal "" (decode "")
+        , skip <|
+            test "encode string with no single characters" <|
+                \() -> Expect.equal "2A3B4C" (encode "AABBBCCCC")
+        , skip <|
+            test "decode string with no single characters" <|
                 \() -> Expect.equal "AABBBCCCC" (decode "2A3B4C")
         , skip <|
-            test "encode with single values" <|
+            test "encode single characters mixed with repeated characters" <|
                 \() ->
                     Expect.equal "12WB12W3B24WB"
                         (encode "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB")
         , skip <|
-            test "decode with single values" <|
+            test "decode single characters with repeated characters" <|
                 \() ->
                     Expect.equal "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
                         (decode "12WB12W3B24WB")
         , skip <|
-            test "(decode (encode (...)) combination" <|
+            test "encode followed by decode gives original string" <|
                 \() ->
                     Expect.equal "zzz ZZ  zZ"
                         (decode (encode "zzz ZZ  zZ"))
