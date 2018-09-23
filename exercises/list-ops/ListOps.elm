1df6c126e11c38a0a444190824589cb960dc7f3e
Kenneth Bogner <tuxagon91@gmail.com>
2018-09-23 19:51:59 -0400

    Update List-Ops exercise example
    Details:
    - Replaced `(%)` with `modBy`
    - Use `Debug.todo` in stub

--- exercises/list-ops/ListOps.elm
+++ exercises/list-ops/ListOps.elm
@@ -12,39 +12,39 @@ module ListOps exposing
 
 length : List a -> Int
 length list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 reverse : List a -> List a
 reverse list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 foldl : (a -> b -> b) -> b -> List a -> b
 foldl f acc list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 foldr : (a -> b -> b) -> b -> List a -> b
 foldr f acc list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 map : (a -> b) -> List a -> List b
 map f list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 filter : (a -> Bool) -> List a -> List a
 filter f list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 append : List a -> List a -> List a
 append xs ys =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
 
 
 concat : List (List a) -> List a
 concat list =
-    Debug.crash "Please implement this function"
+    Debug.todo "Please implement this function"
