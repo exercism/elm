a5e1b59abc015a2b6a7d76b92f78cfba574caba2
Benjamin Reynolds <breyno127@gmail.com>
2018-10-21 13:03:14 -0500

    Update hello-world exercise to new version

--- exercises/hello-world/HelloWorld.example.elm
+++ exercises/hello-world/HelloWorld.example.elm
@@ -1,11 +1,6 @@
 module HelloWorld exposing (helloWorld)
 
 
-helloWorld : Maybe String -> String
-helloWorld name =
-    case name of
-        Just name_ ->
-            "Hello, " ++ name_ ++ "!"
-
-        Nothing ->
-            "Hello, World!"
+helloWorld : String
+helloWorld =
+    "Hello, World!"
