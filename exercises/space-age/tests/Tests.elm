6a45d88b71b874e2f7e3add982d498d4b4d8c818
Michael Glass <me@mike.is>
2019-04-18 11:50:02 +0200

    remove 128 year tests and use within so we can use canonical test numbers

--- exercises/space-age/tests/Tests.elm
+++ exercises/space-age/tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (tests)
 
-import Expect
+import Expect exposing (FloatingPointTolerance(..))
 import SpaceAge exposing (Planet(..), ageOn)
 import Test exposing (..)
 
@@ -9,74 +9,50 @@ tests : Test
 tests =
     describe "SpaceAge"
         [ test "age in earth years" <|
-            \() -> Expect.equal 32 (round (ageOn Earth 1000000000.0))
+            \() -> Expect.within (Absolute 0.01) 31.69 (ageOn Earth 1000000000.0)
         , skip <|
             test "age in mercury years" <|
-                \() -> Expect.equal 281 (round (ageOn Mercury 2134835688.0))
+                \() -> Expect.within (Absolute 0.01) 280.88 (ageOn Mercury 2134835688.0)
         , skip <|
             test "age in venus years" <|
-                \() -> Expect.equal 10 (round (ageOn Venus 189839836.0))
+                \() -> Expect.within (Absolute 0.01) 9.78 (ageOn Venus 189839836.0)
         , skip <|
             test "age on mars" <|
-                \() -> Expect.equal 39 (round (ageOn Mars 2329871239.0))
+                \() -> Expect.within (Absolute 0.01) 35.88 (ageOn Mars 2129871239.0)
         , skip <|
             test "age on jupiter" <|
-                \() -> Expect.equal 2 (round (ageOn Jupiter 901876382.0))
+                \() -> Expect.within (Absolute 0.01) 2.41 (ageOn Jupiter 901876382.0)
         , skip <|
             test "age on saturn" <|
-                \() -> Expect.equal 3 (round (ageOn Saturn 3000000000.0))
+                \() -> Expect.within (Absolute 0.01) 2.15 (ageOn Saturn 2000000000.0)
         , skip <|
             test "age on uranus" <|
-                \() -> Expect.equal 1 (round (ageOn Uranus 3210123456.0))
+                \() -> Expect.within (Absolute 0.01) 0.46 (ageOn Uranus 1210123456.0)
         , skip <|
             test "age on neptune" <|
-                \() -> Expect.equal 2 (round (ageOn Neptune 8210123456.0))
+                \() -> Expect.within (Absolute 0.01) 0.35 (ageOn Neptune 1821023456.0)
         , skip <|
             test "2 earth years in earth years" <|
-                \() -> Expect.equal 2 (round (ageOn Earth 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 2 (ageOn Earth 63072000.0)
         , skip <|
             test "2 earth years in mercury years" <|
-                \() -> Expect.equal 8 (round (ageOn Mercury 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 8.3 (ageOn Mercury 63072000.0)
         , skip <|
             test "2 earth years in venus years" <|
-                \() -> Expect.equal 3 (round (ageOn Venus 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 3.25 (ageOn Venus 63072000.0)
         , skip <|
             test "2 earth years on mars" <|
-                \() -> Expect.equal 1 (round (ageOn Mars 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 1.06 (ageOn Mars 63072000.0)
         , skip <|
             test "2 earth years on jupiter" <|
-                \() -> Expect.equal 0 (round (ageOn Jupiter 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 0.17 (ageOn Jupiter 63072000.0)
         , skip <|
             test "2 earth years on saturn" <|
-                \() -> Expect.equal 0 (round (ageOn Saturn 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 0.07 (ageOn Saturn 63072000.0)
         , skip <|
             test "2 earth years on uranus" <|
-                \() -> Expect.equal 0 (round (ageOn Uranus 63072000.0))
+                \() -> Expect.within (Absolute 0.01) 0.02 (ageOn Uranus 63072000.0)
         , skip <|
             test "2 earth years on neptune" <|
-                \() -> Expect.equal 0 (round (ageOn Neptune 63072000.0))
-        , skip <|
-            test "128 earth years in earth years" <|
-                \() -> Expect.equal 129 (round (ageOn Earth 4036608000.0))
-        , skip <|
-            test "128 earth years in mercury years" <|
-                \() -> Expect.equal 531 (round (ageOn Mercury 4036608000.0))
-        , skip <|
-            test "128 earth years in venus years" <|
-                \() -> Expect.equal 213 (round (ageOn Venus 4036608000.0))
-        , skip <|
-            test "128 earth years on mars" <|
-                \() -> Expect.equal 68 (round (ageOn Mars 4036608000.0))
-        , skip <|
-            test "128 earth years on jupiter" <|
-                \() -> Expect.equal 9 (round (ageOn Jupiter 4036608000.0))
-        , skip <|
-            test "128 earth years on saturn" <|
-                \() -> Expect.equal 4 (round (ageOn Saturn 4036608000.0))
-        , skip <|
-            test "128 earth years on uranus" <|
-                \() -> Expect.equal 1 (round (ageOn Uranus 4036608000.0))
-        , skip <|
-            test "128 earth years on neptune" <|
-                \() -> Expect.equal 1 (round (ageOn Neptune 4036608000.0))
+                \() -> Expect.within (Absolute 0.01) 0.01 (ageOn Neptune 63072000.0)
         ]
