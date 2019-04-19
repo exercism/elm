1e47b018b88bdccca0f3b156c829e8fedfd0fdc3
Michael Glass <me@mike.is>
2019-04-19 14:07:47 +0200

    return Nothing instead of -1

--- exercises/binary-search/BinarySearch.elm
+++ exercises/binary-search/BinarySearch.elm
@@ -3,6 +3,6 @@ module BinarySearch exposing (find)
 import Array exposing (Array)
 
 
-find : Int -> Array Int -> Int
+find : Int -> Array Int -> Maybe Int
 find target xs =
     Debug.todo "Please implement this function"
