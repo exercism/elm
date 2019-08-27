8d5833146f3621f92dead9ae2df878bd7e4206dc
Mert Torun <mert@merttorun.com>
2019-08-27 17:38:42 +0300

    Replace string constants in Allergies with an union type

--- exercises/allergies/tests/Tests.elm
+++ exercises/allergies/tests/Tests.elm
@@ -1,6 +1,6 @@
 module Tests exposing (tests)
 
-import Allergies exposing (isAllergicTo, toList)
+import Allergies exposing (Allergy(..), isAllergicTo, toList)
 import Expect
 import List
 import Test exposing (..)
@@ -12,27 +12,27 @@ tests =
         [ describe "isAllergicTo"
             [ describe "no allergies means not allergic"
                 [ test "peanuts" <|
-                    \() -> Expect.equal False (isAllergicTo "peanuts" 0)
+                    \() -> Expect.equal False (isAllergicTo Peanuts 0)
                 , skip <|
                     test "cats" <|
-                        \() -> Expect.equal False (isAllergicTo "cats" 0)
+                        \() -> Expect.equal False (isAllergicTo Cats 0)
                 , skip <|
                     test "strawberries" <|
-                        \() -> Expect.equal False (isAllergicTo "strawberries" 0)
+                        \() -> Expect.equal False (isAllergicTo Strawberries 0)
                 ]
             , skip <|
                 test "is allergic to eggs" <|
-                    \() -> Expect.equal True (isAllergicTo "eggs" 1)
+                    \() -> Expect.equal True (isAllergicTo Eggs 1)
             , describe "has the right allergies"
                 [ skip <|
                     test "eggs" <|
-                        \() -> Expect.equal True (isAllergicTo "eggs" 5)
+                        \() -> Expect.equal True (isAllergicTo Eggs 5)
                 , skip <|
                     test "shellfish" <|
-                        \() -> Expect.equal True (isAllergicTo "shellfish" 5)
+                        \() -> Expect.equal True (isAllergicTo Shellfish 5)
                 , skip <|
                     test "strawberries" <|
-                        \() -> Expect.equal False (isAllergicTo "strawberries" 5)
+                        \() -> Expect.equal False (isAllergicTo Strawberries 5)
                 ]
             ]
         , describe "toList"
@@ -41,19 +41,51 @@ tests =
                     \() -> Expect.equal [] (toList 0)
             , skip <|
                 test "allergic to just peanuts" <|
-                    \() -> Expect.equal [ "peanuts" ] (toList 2)
+                    \() -> Expect.equal [ Peanuts ] (toList 2)
             , skip <|
                 test "allergic to everything" <|
                     \() ->
-                        Expect.equal (List.sort [ "eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats" ])
-                            (255 |> toList |> List.sort)
+                        Expect.equal (allergySort [ Eggs, Peanuts, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats ])
+                            (255 |> toList |> allergySort)
             , skip <|
                 test "ignore non allergen score parts" <|
-                    \() -> Expect.equal [ "eggs" ] (toList 257)
+                    \() -> Expect.equal [ Eggs ] (toList 257)
             , skip <|
                 test "ignore all non allergen score parts" <|
                     \() ->
-                        Expect.equal (List.sort [ "eggs", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats" ])
-                            (509 |> toList |> List.sort)
+                        Expect.equal (allergySort [ Eggs, Shellfish, Strawberries, Tomatoes, Chocolate, Pollen, Cats ])
+                            (509 |> toList |> allergySort)
             ]
         ]
+
+
+allergySort : List Allergy -> List Allergy
+allergySort =
+    let
+        allergyOrder allergy =
+            case allergy of
+                Eggs ->
+                    1
+
+                Peanuts ->
+                    2
+
+                Shellfish ->
+                    3
+
+                Strawberries ->
+                    4
+
+                Tomatoes ->
+                    5
+
+                Chocolate ->
+                    6
+
+                Pollen ->
+                    7
+
+                Cats ->
+                    8
+    in
+    List.sortBy allergyOrder
