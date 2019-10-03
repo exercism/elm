243665b2af513b9d67f7845ad9077f86298bd380
Leo jPod <leo.jpod@lilo.org>
2019-10-03 21:07:12 +0200

    hop, skip test and prepare the stub

--- exercises/armstrong-number/Armstrong.elm
+++ exercises/armstrong-number/Armstrong.elm
@@ -1,26 +1,6 @@
 module Armstrong exposing (isArmstrongNumber)
 
-import List
-
 
 isArmstrongNumber : Int -> Bool
 isArmstrongNumber n =
-    numberToDigits n []
-        |> (\digits ->
-                List.map (\d -> d ^ List.length digits) digits
-           )
-        |> List.sum
-        |> (==) n
-
-
-
--- Debug.todo "Please implement this function"
-
-
-numberToDigits : Int -> List Int -> List Int
-numberToDigits n list =
-    if n // 10 == 0 then
-        n :: list
-
-    else
-        numberToDigits (n // 10) <| remainderBy 10 n :: list
+    Debug.todo "Please implement this function"
--- exercises/armstrong-number/tests/Tests.elm
+++ exercises/armstrong-number/tests/Tests.elm
@@ -12,36 +12,44 @@ tests =
             \_ ->
                 Expect.equal True <|
                     isArmstrongNumber 0
-        , test "Single digit numbers are Armstrong numbers" <|
-            \_ ->
-                Expect.equal True <|
-                    isArmstrongNumber 5
-        , test "There are no 2 digit Armstrong numbers" <|
-            \_ ->
-                Expect.equal False <|
-                    isArmstrongNumber 10
-        , test "Three digit number that is an Armstrong number" <|
-            \_ ->
-                Expect.equal True <|
-                    isArmstrongNumber 153
-        , test "Three digit number that is not an Armstrong number" <|
-            \_ ->
-                Expect.equal False <|
-                    isArmstrongNumber 100
-        , test "Four digit number that is an Armstrong number" <|
-            \_ ->
-                Expect.equal True <|
-                    isArmstrongNumber 9474
-        , test "Four digit number that is not an Armstrong number" <|
-            \_ ->
-                Expect.equal False <|
-                    isArmstrongNumber 9475
-        , test "Seven digit number that is an Armstrong number" <|
-            \_ ->
-                Expect.equal True <|
-                    isArmstrongNumber 9926315
-        , test "Seven digit number that is not an Armstrong number" <|
-            \_ ->
-                Expect.equal False <|
-                    isArmstrongNumber 9926314
+        , skip <|
+            test "Single digit numbers are Armstrong numbers" <|
+                \_ ->
+                    Expect.equal True <|
+                        isArmstrongNumber 5
+        , skip <|
+            test "There are no 2 digit Armstrong numbers" <|
+                \_ ->
+                    Expect.equal False <|
+                        isArmstrongNumber 10
+        , skip <|
+            test "Three digit number that is an Armstrong number" <|
+                \_ ->
+                    Expect.equal True <|
+                        isArmstrongNumber 153
+        , skip <|
+            test "Three digit number that is not an Armstrong number" <|
+                \_ ->
+                    Expect.equal False <|
+                        isArmstrongNumber 100
+        , skip <|
+            test "Four digit number that is an Armstrong number" <|
+                \_ ->
+                    Expect.equal True <|
+                        isArmstrongNumber 9474
+        , skip <|
+            test "Four digit number that is not an Armstrong number" <|
+                \_ ->
+                    Expect.equal False <|
+                        isArmstrongNumber 9475
+        , skip <|
+            test "Seven digit number that is an Armstrong number" <|
+                \_ ->
+                    Expect.equal True <|
+                        isArmstrongNumber 9926315
+        , skip <|
+            test "Seven digit number that is not an Armstrong number" <|
+                \_ ->
+                    Expect.equal False <|
+                        isArmstrongNumber 9926314
         ]
