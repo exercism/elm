a7151d4d294549d5d134891a99362fedce15c068
Cedd Burge <ceddlyburge@gmail.com>
2021-05-14 09:02:50 +0100

    Add initial elm template for list concept exercise

---
+++ exercises/concept/tracks-on-tracks-on-tracks/src/TracksOnTracksOnTracks.elm
@@ -0,0 +1,19 @@
+module TracksOnTracksOnTracks exposing (..)
+
+newList = 
+    Debug.todo "implement this function and create a type annotation"
+
+existingList = 
+    Debug.todo "implement this function and create a type annotation"
+
+addLanguage language languages = 
+    Debug.todo "implement this function and create a type annotation"
+
+countLanguages languages = 
+    Debug.todo "implement this function and create a type annotation"
+
+reverseList languages = 
+    Debug.todo "implement this function and create a type annotation"
+
+excitingList languages =
+    Debug.todo "implement this function and create a type annotation"
