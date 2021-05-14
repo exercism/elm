ccf7f3ed5cf5bd65f914d1ca68a4502d8d2ecf2f
Cedd Burge <ceddlyburge@gmail.com>
2021-05-14 08:55:10 +0100

    Add exemplar solution for list concept exercise

---
+++ exercises/concept/tracks-on-tracks-on-tracks/.meta/Exemplar.elm
@@ -0,0 +1,38 @@
+module TracksOnTracksOnTracks exposing(..)
+
+import List as List exposing (List)
+
+newList: List a
+newList = 
+    []
+
+existingList : List String
+existingList = 
+    [ "Elm", "Clojure", "Haskell" ]
+
+addLanguage : String -> List String -> List String
+addLanguage language languages = 
+    language :: languages
+
+countLanguages : List a -> Int
+countLanguages languages = 
+    List.length languages
+
+reverseList : List String -> List String
+reverseList languages = 
+    List.reverse languages
+
+excitingList List String -> Bool
+excitingList languages =
+    case languages of
+        "Elm" :: _ -> 
+            True
+        
+        [ _; "Elm" ] -> 
+            True
+
+        [ _; "Elm"; _ ] -> 
+            True
+        
+        _ -> 
+            False
