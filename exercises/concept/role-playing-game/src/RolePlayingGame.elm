45852ed3ab42ecb9f933a9c212c9d12cfe7c7d38
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-05-28 11:25:06 +0200

    Move exemplar code to .meta

--- exercises/concept/role-playing-game/src/RolePlayingGame.elm
+++ exercises/concept/role-playing-game/src/RolePlayingGame.elm
@@ -10,25 +10,9 @@ type alias Player =
 
 revive : Player -> Maybe Player
 revive player =
-    if player.health > 0 then
-        Nothing
-
-    else if player.level >= 10 then
-        Just { player | health = 100, mana = Just 100 }
-
-    else
-        Just { player | health = 100, mana = Nothing }
+    Debug.todo "Please implement this function"
 
 
 castSpell : Int -> Player -> ( Player, Int )
 castSpell manaCost player =
-    case player.mana of
-        Nothing ->
-            ( { player | health = max 0 (player.health - manaCost) }, 0 )
-
-        Just mana ->
-            if mana >= manaCost then
-                ( { player | mana = Just (mana - manaCost) }, 2 * manaCost )
-
-            else
-                ( player, 0 )
+    Debug.todo "Please implement this function"
