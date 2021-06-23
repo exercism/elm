18476471b727784311fd3ace3516212157cd81d8
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-06-23 14:25:09 +0200

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ echo '-------------------------------------------------------'
 echo "Checking Formatting"
 
 if [[ ! $(npx --no-install elm-format --help | grep "elm-format 0.8.5") ]]; then
-  echo "Please run npm install first"
+  echo "elm-format not found or wrong version, please run `elm-tooling install` first."
   exit 1
 fi
 
@@ -18,11 +18,9 @@ npx --no-install elm-format --yes --validate \
 
 if [ $? -ne 0 ]; then
   echo "*******************************************************************"
-  echo "*******************************************************************"
   echo "**                       elm-format failed                       **"
   echo "**        perhaps some of your changes are not formatted?        **"
   echo "**             Please run elm-format before pushing.             **"
-  echo "*******************************************************************"
   echo "*******************************************************************"
   exit 1
 else
@@ -46,6 +44,7 @@ do
   exercise_name=$(basename $(ls $exercise_dir/src/*.elm) .elm)
   # TODO: check that all exercise_name are unique
   cp $example_file "build/src/$exercise_name.elm"
+  # Copy tests files under a unique temporary directory and remove all "skip <| ..."
   cat "$exercise_dir/tests/Tests.elm" | sed "s/module Tests/module Tests$exercise_name/" | sed 's/skip <|//g' > "build/tests/Tests$exercise_name.elm"
 done
 
@@ -64,6 +63,7 @@ do
   exercise_dir=$(dirname $(dirname $(dirname $example_file)))
   exercise_name=$(basename $example_file .example.elm)
   cp $example_file "build/src/$exercise_name.elm"
+  # Copy tests files under a unique temporary directory and remove all "skip <| ..."
   cat "$exercise_dir/tests/Tests.elm" | sed "s/module Tests/module Tests$exercise_name/" | sed 's/skip <|//g' > "build/tests/Tests$exercise_name.elm"
 done
 
