b5a98bcbca6c8a431350ee678dfa3d5a08836fd8
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2021-07-01 00:10:08 +0200

    Use elm-tooling to remove the JS ecosystem from this repo (#378)
    * Use elm-test-rs in bin/build.sh
    
    * Remove package.json and package-lock.json
    
    * Add elm-tooling.json with elm, elm-format, elm-test-rs
    
    * Updated CI
    
    * Update build.sh
    
    * Fix CI triggers
    
    * Node 10 is eol, switching with 16 for CI
    
    * Fix multiple cache usage on CI

--- bin/build.sh
+++ bin/build.sh
@@ -6,7 +6,7 @@ echo '-------------------------------------------------------'
 echo "Checking Formatting"
 
 if [[ ! $(npx --no-install elm-format --help | grep "elm-format 0.8.5") ]]; then
-  echo "Please run npm install first"
+  echo "elm-format not found or wrong version, please run `elm-tooling install` first."
   exit 1
 fi
 
@@ -18,11 +18,9 @@ npx --no-install elm-format --yes --validate \
 
 if [ $? -ne 0 ]; then
   echo "*******************************************************************"
-  echo "*******************************************************************"
   echo "**                       elm-format failed                       **"
   echo "**        perhaps some of your changes are not formatted?        **"
   echo "**             Please run elm-format before pushing.             **"
-  echo "*******************************************************************"
   echo "*******************************************************************"
   exit 1
 else
@@ -46,10 +44,11 @@ do
   exercise_name=$(basename $(ls $exercise_dir/src/*.elm) .elm)
   # TODO: check that all exercise_name are unique
   cp $example_file "build/src/$exercise_name.elm"
+  # Copy tests files under a unique temporary directory and remove all "skip <| ..."
   cat "$exercise_dir/tests/Tests.elm" | sed "s/module Tests/module Tests$exercise_name/" | sed 's/skip <|//g' > "build/tests/Tests$exercise_name.elm"
 done
 
-cd build && npx --no-install elm-test --fuzz 10
+cd build && npx --no-install elm-test-rs --fuzz 10
 cd ..
 
 # TEST (practice)
@@ -64,8 +63,9 @@ do
   exercise_dir=$(dirname $(dirname $(dirname $example_file)))
   exercise_name=$(basename $example_file .example.elm)
   cp $example_file "build/src/$exercise_name.elm"
+  # Copy tests files under a unique temporary directory and remove all "skip <| ..."
   cat "$exercise_dir/tests/Tests.elm" | sed "s/module Tests/module Tests$exercise_name/" | sed 's/skip <|//g' > "build/tests/Tests$exercise_name.elm"
 done
 
-cd build && npx --no-install elm-test --fuzz 10
+cd build && npx --no-install elm-test-rs --fuzz 10
 cd ..
