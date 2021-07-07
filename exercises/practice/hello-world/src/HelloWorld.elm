d41c2f42159cfe5040832ddf972507d31109af68
Erik Schierboom <erik_schierboom@hotmail.com>
2021-07-07 13:24:54 +0200

    Simplify the `hello-world` exercise's stub. The goal of this is to make things easier for students to get started and to introduce as little syntax as possible.

--- exercises/practice/hello-world/src/HelloWorld.elm
+++ exercises/practice/hello-world/src/HelloWorld.elm
@@ -1,22 +1,6 @@
-{-
-   This is a "stub" file.  It's a little start on your solution. It's not a
-   complete solution though; you have to write some code.
-
-   The module name is expected by the test program and must match the name of this
-   file. It has to stay just the way it is.
--}
-
-
 module HelloWorld exposing (helloWorld)
 
 
+helloWorld : String
 helloWorld =
-    "Please implement this function"
-
-
-
-{-
-   When you have a working solution, REMOVE ALL THE STOCK COMMENTS.
-   They're here to help you get started but they only clutter a finished solution.
-   If you leave them in, nitpickers will protest!
--}
+    "Goodbye, Mars!"
