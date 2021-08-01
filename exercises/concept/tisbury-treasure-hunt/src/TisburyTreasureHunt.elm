005e401c29c1c91e581b7ac5ab88c60015a41756
Cedd Burge <ceddlyburge@gmail.com>
2021-08-01 20:40:05 +0100

    Add instructions, and modify tests / code to match

--- exercises/concept/tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
+++ exercises/concept/tisbury-treasure-hunt/src/TisburyTreasureHunt.elm
@@ -1,22 +1,25 @@
 module TisburyTreasureHunt exposing (..)
 
--- Consider defining a type alias for TreasureLocation, 
--- Treasure, Placelocation and Place, 
+-- Consider defining a type alias for TreasureLocation,
+-- Treasure, Placelocation and Place,
 -- and using them in the function type annotations
 
 
-placeLocationToTreasureLocation: (Char, Int) -> (Int, Char)
+placeLocationToTreasureLocation : ( Char, Int ) -> ( Int, Char )
 placeLocationToTreasureLocation placeLocation =
     Debug.todo "implement this function"
 
-treasureLocationMatchesPlaceLocation: (Int, Char) -> (Char, Int) -> Bool
-treasureLocationMatchesPlaceLocation treasureLocation placeLocation =
+
+treasureLocationMatchesPlaceLocation : ( Char, Int ) -> ( Int, Char ) -> Bool
+treasureLocationMatchesPlaceLocation placeLocation treasureLocation =
     Debug.todo "implement this function"
 
-countTreasureLocations: (String, TreasureLocation) -> List (String, PlaceLocation) -> Int
-countTreasureLocations (_, treasureLocation) places =
+
+countPlaceTreasures : ( String, (Char, Int) ) -> List ( String, (Int, Char) ) -> Int
+countPlaceTreasures place treasures =
     Debug.todo "implement this function"
 
-specialCaseSwapPossible: (String, TreasureLocation) -> (String, PlaceLocation) -> (String, TreasureLocation) -> Bool
-specialCaseSwapPossible (foundTreasure, _) (place, _) (desiredTreasure, _) =
+
+specialCaseSwapPossible : ( String, TreasureLocation ) -> ( String, PlaceLocation ) -> ( String, TreasureLocation ) -> Bool
+specialCaseSwapPossible ( foundTreasure, _ ) ( place, _ ) ( desiredTreasure, _ ) =
     Debug.todo "implement this function"
--- exercises/concept/tisbury-treasure-hunt/tests/Tests.elm
+++ exercises/concept/tisbury-treasure-hunt/tests/Tests.elm
@@ -1,8 +1,8 @@
 module Tests exposing (tests)
 
-import TisburyTreasureHunt exposing (..)
 import Expect exposing (FloatingPointTolerance(..))
 import Test exposing (..)
+import TisburyTreasureHunt exposing (..)
 
 
 tests : Test
@@ -10,78 +10,78 @@ tests =
     describe "TisburyTreasureHunt"
         [ test "placeLocationToTreasureLocation should convert PlaceLocation to TreasureLocation" <|
             \_ ->
-                placeLocationToTreasureLocation ('C', 1) 
-                    |> Expect.equal (1, 'C')
+                placeLocationToTreasureLocation ( 'C', 1 )
+                    |> Expect.equal ( 1, 'C' )
         , test "1,F is not at Seaside Cottages" <|
             \_ ->
-                treasureLocationMatchesPlaceLocation (1, 'F') ('C', 1)
+                treasureLocationMatchesPlaceLocation ( 'C', 1 ) ( 1, 'F' )
                     |> Expect.equal False
         , test "1, F is at Aqua Lagoon" <|
             \_ ->
-                treasureLocationMatchesPlaceLocation (1, 'F') ('F', 1)
+                treasureLocationMatchesPlaceLocation ( 'F', 1 ) ( 1, 'F' )
                     |> Expect.equal True
-        , test "treasure should be matched with locations where they exist" <|
+        , test "places should know how many treasures are available" <|
             \_ ->
-                 countTreasureLocations 
-                    ("Amethyst Octopus", (1, 'F'))
-                    [ ("Aqua Lagoon (Island of Mystery)", ('F', 1))
-                    , ("Foggy Seacave", ('C', 8))
+                countPlaceTreasures
+                    ( "Aqua Lagoon (Island of Mystery)", ( 'F', 1 ) )
+                    [ ( "Amethyst Octopus", ( 1, 'F' ) )
+                    , ( "Scrimshaw Whale's Tooth", ( 1, 'F' ) )
                     ]
-                |> Expect.equal 1
+                    |> Expect.equal 2
         , test "can swap Amethyst Octopus for Crystal Crab at Stormy Breakwater" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Amethyst Octopus", (1, 'F'))
-                    ("Stormy Breakwater", ('B', 5))
-                    ("Crystal Crab", (6, 'A'))
-                |> Expect.equal True
+                specialCaseSwapPossible
+                    ( "Amethyst Octopus", ( 1, 'F' ) )
+                    ( "Stormy Breakwater", ( 'B', 5 ) )
+                    ( "Crystal Crab", ( 6, 'A' ) )
+                    |> Expect.equal True
         , test "can swap Amethyst Octopus for Glass Starfish at Stormy Breakwater" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Amethyst Octopus", (1, 'F'))
-                    ("Stormy Breakwater", ('B', 5))
-                    ("Glass Starfish", (6, 'D'))
-                |> Expect.equal True
+                specialCaseSwapPossible
+                    ( "Amethyst Octopus", ( 1, 'F' ) )
+                    ( "Stormy Breakwater", ( 'B', 5 ) )
+                    ( "Glass Starfish", ( 6, 'D' ) )
+                    |> Expect.equal True
         , test "cannot swap Amethyst Octopus for Angry Monkey Figurine at Stormy Breakwater" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Amethyst Octopus", (1, 'F'))
-                    ("Stormy Breakwater", ('B', 5))
-                    ("Angry Monkey Figurine", (5, 'B'))
-                |> Expect.equal False
+                specialCaseSwapPossible
+                    ( "Amethyst Octopus", ( 1, 'F' ) )
+                    ( "Stormy Breakwater", ( 'B', 5 ) )
+                    ( "Angry Monkey Figurine", ( 5, 'B' ) )
+                    |> Expect.equal False
         , test "can swap Vintage Pirate Hat for Model Ship in Large Bottle at Harbor Managers Office" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Vintage Pirate Hat", (7, 'E'))
-                    ("Harbor Managers Office", ('A', 8))
-                    ("Model Ship in Large Bottle", (8, 'A'))
-                |> Expect.equal True
+                specialCaseSwapPossible
+                    ( "Vintage Pirate Hat", ( 7, 'E' ) )
+                    ( "Harbor Managers Office", ( 'A', 8 ) )
+                    ( "Model Ship in Large Bottle", ( 8, 'A' ) )
+                    |> Expect.equal True
         , test "can swap Vintage Pirate Hat for Antique Glass Fishnet Float at Harbor Managers Office" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Vintage Pirate Hat", (7, 'E'))
-                    ("Harbor Managers Office", ('A', 8))
-                    ("Antique Glass Fishnet Float", (3, 'D'))
-                |> Expect.equal True
+                specialCaseSwapPossible
+                    ( "Vintage Pirate Hat", ( 7, 'E' ) )
+                    ( "Harbor Managers Office", ( 'A', 8 ) )
+                    ( "Antique Glass Fishnet Float", ( 3, 'D' ) )
+                    |> Expect.equal True
         , test "cannot swap Vintage Pirate Hat for Carved Wooden Elephant at Harbor Managers Office" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Vintage Pirate Hat", (7, 'E'))
-                    ("Harbor Managers Office", ('A', 8))
-                    ("Carved Wooden Elephant", (8, 'C'))
-                |> Expect.equal False
+                specialCaseSwapPossible
+                    ( "Vintage Pirate Hat", ( 7, 'E' ) )
+                    ( "Harbor Managers Office", ( 'A', 8 ) )
+                    ( "Carved Wooden Elephant", ( 8, 'C' ) )
+                    |> Expect.equal False
         , test "can swap Brass Spyglass for Robot Parrot at Abandoned Lighthouse" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Brass Spyglass", (4, 'B'))
-                    ("Abandoned Lighthouse", ('B', 4))
-                    ("Robot Parrot", (1, 'C'))
-                |> Expect.equal True
+                specialCaseSwapPossible
+                    ( "Brass Spyglass", ( 4, 'B' ) )
+                    ( "Abandoned Lighthouse", ( 'B', 4 ) )
+                    ( "Robot Parrot", ( 1, 'C' ) )
+                    |> Expect.equal True
         , test "cannot swap Vintage Pirate Hat for Model Ship in Large Bottle at Old Schooner" <|
             \_ ->
-                 specialCaseSwapPossible 
-                    ("Vintage Pirate Hat", (7, 'E'))
-                    ("Old Schooner", ('A', 6))
-                    ("Model Ship in Large Bottle", (8, 'A'))
-                |> Expect.equal False
+                specialCaseSwapPossible
+                    ( "Vintage Pirate Hat", ( 7, 'E' ) )
+                    ( "Old Schooner", ( 'A', 6 ) )
+                    ( "Model Ship in Large Bottle", ( 8, 'A' ) )
+                    |> Expect.equal False
         ]
