500d55d27d5c2d4cfcb33a076fac0a1d133282f6
Cedd Burge <ceddlyburge@gmail.com>
2021-11-01 20:34:46 +0000

    Add Go / Result concept exercise code

---
+++ exercises/concept/result/src/Go.elm
@@ -0,0 +1,40 @@
+module Go exposing (..)
+
+
+type Player
+    = Black
+    | White
+
+
+type alias Game =
+    { whiteCapturedStones : Int
+    , blackCapturedStones : Int
+    , player : Player
+    , error : String
+    }
+
+
+type alias Rule =
+    Game -> Result String Game
+
+
+type alias NonValidatingRule =
+    Game -> Game
+
+
+changePlayer : Game -> Game
+changePlayer game =
+    let
+        newPlayer =
+            if game.player == White then
+                Black
+
+            else
+                White
+    in
+    { game | player = newPlayer }
+
+
+applyRules : Game -> Rule -> NonValidatingRule -> Rule -> Rule -> Game
+applyRules game oneStonePerPointRule captureRule libertyRule koRule =
+    Debug.todo "Please implement this function"
