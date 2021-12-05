acda4654c18dddcdd58fca51fa82ca5d37addb28
Cedd Burge <ceddlyburge@gmail.com>
2021-12-05 15:33:42 +0000

    Convert example to use Maybe instead of Result

--- exercises/practice/bowling/.meta/src/Bowling.example.elm
+++ exercises/practice/bowling/.meta/src/Bowling.example.elm
@@ -19,65 +19,72 @@ type FinalFrame
     | FinalStrike (Maybe Int) (Maybe Int)
 
 
-score : Rolls -> Result String Int
+score : Rolls -> Maybe Int
 score =
-    List.foldl (\roll -> Result.andThen (\frames -> update frames roll)) (Ok [])
-        >> Result.andThen
+    List.foldl (\roll -> Maybe.andThen (\frames -> update frames roll)) (Just [])
+        >> Maybe.andThen
             (\frames ->
                 case frames of
                     (FinalFrame finalFrame) :: otherFrames ->
                         List.foldl
                             (\frame ->
-                                Result.andThen
+                                Maybe.andThen
                                     (\( points, rollPlus1, rollPlus2 ) ->
                                         case frame of
                                             FinalFrame _ ->
-                                                Err "only the last frame can be the final one"
+                                                -- "only the last frame can be the final one"
+                                                Nothing
 
                                             Incomplete _ ->
-                                                Err "cannot score when a frame is incomplete"
+                                                -- "cannot score when a frame is incomplete"
+                                                Nothing
 
                                             Open rollA rollB ->
-                                                Ok ( points + rollA + rollB, rollA, rollB )
+                                                Just ( points + rollA + rollB, rollA, rollB )
 
                                             Spare secondRoll ->
-                                                Ok ( points + 10 + rollPlus1, 10 - secondRoll, secondRoll )
+                                                Just ( points + 10 + rollPlus1, 10 - secondRoll, secondRoll )
 
                                             Strike ->
-                                                Ok ( points + 10 + rollPlus1 + rollPlus2, 10, rollPlus1 )
+                                                Just ( points + 10 + rollPlus1 + rollPlus2, 10, rollPlus1 )
                                     )
                             )
                             (case finalFrame of
                                 FinalOpen rollA rollB ->
-                                    Ok ( rollA + rollB, rollA, rollB )
+                                    Just ( rollA + rollB, rollA, rollB )
 
                                 FinalSpare secondRoll (Just fillRoll) ->
-                                    Ok ( 10 + fillRoll, 10 - secondRoll, secondRoll )
+                                    Just ( 10 + fillRoll, 10 - secondRoll, secondRoll )
 
                                 FinalStrike (Just fillRollA) (Just fillRollB) ->
-                                    Ok ( 10 + fillRollA + fillRollB, 10, fillRollA )
+                                    Just ( 10 + fillRollA + fillRollB, 10, fillRollA )
 
                                 _ ->
-                                    Err "cannot score before rolling entirely the final frame"
+                                    -- "cannot scßore before rolling entirely the final frame"
+                                    Nothing
                             )
                             otherFrames
 
                     _ ->
-                        Err "cannot score incomplete game"
+                        -- "cannot score incomplete game"
+                        Nothing
             )
-        >> Result.map (\( points, _, _ ) -> points)
+        >> Maybe.map (\( points, _, _ ) -> points)
 
 
-update : List Frame -> Int -> Result String (List Frame)
+update : List Frame -> Int -> Maybe (List Frame)
 update reversedFrames newRoll =
     if newRoll < 0 then
-        Err "cannot make less than 0 point on a roll"
+        -- "cannot make less than 0 point on Maybe. roll"
+        Nothing
 
     else if newRoll > 10 then
-        Err "cannot score more than 10 points on a roll"
+        -- "cannot score more than 10 points on a roll"
+        Nothing
 
     else if List.length reversedFrames > 10 then
-        Err "cannot score more than 10 frames"
+        -- "cannot score more than 10 frames"
+        Nothing
 
     else if List.length reversedFrames == 10 then
         -- dealing with the last frame here
@@ -88,29 +95,32 @@ update reversedFrames newRoll =
                         firstRoll + newRoll
                 in
                 if totalFrame > 10 then
-                    Err "cannot score more than 10 points in 2 rolls"
+                    -- "cannot score more than 10 points in 2 rolls"
+                    Nothing
 
                 else if totalFrame == 10 then
-                    Ok <| FinalFrame (FinalSpare newRoll Nothing) :: restOfFrames
+                    Just <| FinalFrame (FinalSpare newRoll Nothing) :: restOfFrames
 
                 else
-                    Ok <| FinalFrame (FinalOpen firstRoll newRoll) :: restOfFrames
+                    Just <| FinalFrame (FinalOpen firstRoll newRoll) :: restOfFrames
 
             (FinalFrame (FinalSpare secondRoll Nothing)) :: restOfFrames ->
-                Ok <| FinalFrame (FinalSpare secondRoll (Just newRoll)) :: restOfFrames
+                Just <| FinalFrame (FinalSpare secondRoll (Just newRoll)) :: restOfFrames
 
             (FinalFrame (FinalStrike Nothing Nothing)) :: restOfFrames ->
-                Ok <| FinalFrame (FinalStrike (Just newRoll) Nothing) :: restOfFrames
+                Just <| FinalFrame (FinalStrike (Just newRoll) Nothing) :: restOfFrames
 
             (FinalFrame (FinalStrike (Just firstFill) Nothing)) :: restOfFrames ->
                 if firstFill /= 10 && firstFill + newRoll > 10 then
-                    Err "cannot score more than 10 points without making a strike"
+                    -- "cannot score more than 10 points without making a strike"
+                    Nothing
 
                 else
-                    Ok <| FinalFrame (FinalStrike (Just firstFill) (Just newRoll)) :: restOfFrames
+                    Just <| FinalFrame (FinalStrike (Just firstFill) (Just newRoll)) :: restOfFrames
 
             _ ->
-                Err "trying to cheat by adding rolls are we?"
+                -- "trying to cheat by adding rolls are we?"
+                Nothing
 
     else
         case reversedFrames of
@@ -120,21 +130,22 @@ update reversedFrames newRoll =
                         firstRoll + newRoll
                 in
                 if totalFrame > 10 then
-                    Err "cannot score more than 10 points in 2 rolls"
+                    -- "cannot score more than 10 points in 2 rolls"
+                    Nothing
 
                 else if totalFrame == 10 then
-                    Ok <| Spare newRoll :: restOfFrames
+                    Just <| Spare newRoll :: restOfFrames
 
                 else
-                    Ok <| Open firstRoll newRoll :: restOfFrames
+                    Just <| Open firstRoll newRoll :: restOfFrames
 
             restOfFrames ->
                 case ( newRoll == 10, List.length reversedFrames == 9 ) of
                     ( True, False ) ->
-                        Ok <| Strike :: restOfFrames
+                        Just <| Strike :: restOfFrames
 
                     ( True, True ) ->
-                        Ok <| FinalFrame (FinalStrike Nothing Nothing) :: restOfFrames
+                        Just <| FinalFrame (FinalStrike Nothing Nothing) :: restOfFrames
 
                     _ ->
-                        Ok <| Incomplete newRoll :: restOfFrames
+                        Just <| Incomplete newRoll :: restOfFrames
