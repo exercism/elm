cfe91edbf600a2a064bd30caa1129b52ebd2c010
Jérémie Gillet <jie.gillet@gmail.com>
2022-02-04 22:11:57 +0900

    Sync: update `accumulate` with configlet

--- exercises/practice/accumulate/.meta/src/Accumulate.example.elm
+++ exercises/practice/accumulate/.meta/src/Accumulate.example.elm
@@ -2,5 +2,5 @@ module Accumulate exposing (accumulate)
 
 
 accumulate : (a -> b) -> List a -> List b
-accumulate func input =
-    List.foldr (\v c -> func v :: c) [] input
+accumulate =
+    List.map
