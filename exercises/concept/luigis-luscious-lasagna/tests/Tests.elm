6a44c62974e10ea7e5d3afc025677e6a3f8ba622
Jérémie Gillet <jie.gillet@gmail.com>
2022-03-26 21:53:40 +0900

    Fix top level describe

--- exercises/concept/luigis-luscious-lasagna/tests/Tests.elm
+++ exercises/concept/luigis-luscious-lasagna/tests/Tests.elm
@@ -7,15 +7,15 @@ import Test exposing (..)
 
 tests : Test
 tests =
-    [ describe "1"
-        [ test "For a 3-layers lasagna started 10 minutes ago, there are 36 minutes remaining" <|
-            \_ ->
-                remainingTimeInMinutes 3 10
-                    |> Expect.equal ((3 * 2) + 40 - 10)
-        , test "For a 6-layers lasagna started 30 minutes ago, there are 22 minutes remaining" <|
-            \_ ->
-                remainingTimeInMinutes 6 30
-                    |> Expect.equal ((6 * 2) + 40 - 30)
+    describe "LuigisLusciousLasagna"
+        [ describe "1"
+            [ test "For a 3-layers lasagna started 10 minutes ago, there are 36 minutes remaining" <|
+                \_ ->
+                    remainingTimeInMinutes 3 10
+                        |> Expect.equal ((3 * 2) + 40 - 10)
+            , test "For a 6-layers lasagna started 30 minutes ago, there are 22 minutes remaining" <|
+                \_ ->
+                    remainingTimeInMinutes 6 30
+                        |> Expect.equal ((6 * 2) + 40 - 30)
+            ]
         ]
-    ]
-        |> describe "LuigisLusciousLasagna"
