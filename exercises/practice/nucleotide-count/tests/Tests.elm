1e754ede777fa71adb71659ccb4b21f298f143e3
NyuB <decaeste@etud.insa-toulouse.fr>
2022-08-12 17:54:08 +0200

    nucleotid-count: Convert tests and samples to new specification Fix#483

--- exercises/practice/nucleotide-count/tests/Tests.elm
+++ exercises/practice/nucleotide-count/tests/Tests.elm
@@ -8,18 +8,24 @@ import Test exposing (..)
 tests : Test
 tests =
     describe "NucleotideCount"
-        [ test "empty dna strand has no nucleotides" <|
+        [ test "empty strand" <|
             \() ->
-                Expect.equal { a = 0, t = 0, c = 0, g = 0 }
+                Expect.equal (Ok { a = 0, t = 0, c = 0, g = 0 })
                     (nucleotideCounts "")
-        , skip <|
-            test "repetitive sequence has only guanine" <|
-                \() ->
-                    Expect.equal { a = 0, t = 0, c = 0, g = 8 }
-                        (nucleotideCounts "GGGGGGGG")
-        , skip <|
-            test "counts all nucleotides" <|
-                \() ->
-                    Expect.equal { a = 20, t = 21, c = 12, g = 17 }
-                        (nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+        , test "can count one nucleotide in single-character input" <|
+            \() ->
+                Expect.equal (Ok { a = 0, t = 0, c = 0, g = 1 })
+                    (nucleotideCounts "G")
+        , test "strand with repeated nucleotide" <|
+            \() ->
+                Expect.equal (Ok { a = 0, t = 0, c = 0, g = 7 })
+                    (nucleotideCounts "GGGGGGG")
+        , test "strand with multiple nucleotides" <|
+            \() ->
+                Expect.equal (Ok { a = 20, t = 21, c = 12, g = 17 })
+                    (nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC")
+        , test "strand with invalid nucleotides" <|
+            \() ->
+                Expect.equal (Err "Invalid nucleotide in strand")
+                    (nucleotideCounts "AGXXACT")
         ]
