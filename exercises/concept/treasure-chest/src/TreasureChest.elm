e7f113615572172535e59bafde53872dceda4173
Cedd Burge <ceddlyburge@gmail.com>
2022-11-20 16:29:00 +0000

    Add raw getTreasure to make the test pass for task 1 when it is done

--- exercises/concept/treasure-chest/src/TreasureChest.elm
+++ exercises/concept/treasure-chest/src/TreasureChest.elm
@@ -1,13 +1,28 @@
 module TreasureChest exposing (..)
 
--- 1. define a single variant generic TreasureChest custom type with associated data:
--- - a String (for a password)
--- - a generic (for the treasure)
--- 2. define a getTreasure function, with parameters:
--- - a String (for the passwordAttempt)
--- - a TreasureChest custom type
---  and returns:
--- - if the passwordAttempt does not match the String in the TreasureChest
---   - Nothing
--- - if the passwordAttempt matches the String in the TreasureChest
---   - Just treasure (where treasure is the generic in the TreasureChest)
+{-|
+
+1.  define a single variant generic TreasureChest custom type with associated data:
+      - a String (for a password)
+
+      - a generic (for the treasure)
+
+-}
+
+
+{-|
+
+1.  define getTreasure
+
+-}
+getTreasure passwordAttempt =
+    Debug.todo """
+define getTreasure, with a type annotation, with two parameters:
+- a String (for the passwordAttempt)
+- a TreasureChest custom type
+ and returns:
+- if the passwordAttempt does not match the String in the TreasureChest
+  - Nothing
+- if the passwordAttempt matches the String in the TreasureChest
+  - Just treasure (where treasure is the generic in the TreasureChest)
+"""
