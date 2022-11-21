c67c340a6b8a7ac7a2491665c0b63727c28f6a54
Cedd Burge <ceddlyburge@gmail.com>
2022-11-21 17:12:25 +0000

    Improve comments

--- exercises/concept/treasure-chest/src/TreasureChest.elm
+++ exercises/concept/treasure-chest/src/TreasureChest.elm
@@ -1,28 +1,28 @@
 module TreasureChest exposing (..)
 
-{-|
+{-| One. define a single variant generic TreasureChest custom type with associated data:
 
-1.  define a single variant generic TreasureChest custom type with associated data:
-      - a String (for a password)
+  - a String (for a password)
 
-      - a generic (for the treasure)
+        - a generic (for the treasure)
 
 -}
 
 
-{-|
+type TreasureChest
+    = TreasureChest
 
-1.  define getTreasure
+
+{-| Two. define getTreasure, with a type annotation, with two parameters:
+
+  - a String (for the passwordAttempt)
+  - a TreasureChest custom type
+    and returns:
+  - if the passwordAttempt does not match the String in the TreasureChest
+      - Nothing
+  - if the passwordAttempt matches the String in the TreasureChest
+      - Just treasure (where treasure is the generic in the TreasureChest)
 
 -}
 getTreasure passwordAttempt =
-    Debug.todo """
-define getTreasure, with a type annotation, with two parameters:
-- a String (for the passwordAttempt)
-- a TreasureChest custom type
- and returns:
-- if the passwordAttempt does not match the String in the TreasureChest
-  - Nothing
-- if the passwordAttempt matches the String in the TreasureChest
-  - Just treasure (where treasure is the generic in the TreasureChest)
-"""
+    Debug.todo "Please implement getTreasure"
