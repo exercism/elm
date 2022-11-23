7e01ba8910829d8e25289c2448eff1e94788dce8
Jie <jie.gillet@gmail.com>
2022-11-23 22:36:45 +0900

    Add new practice exercise `zebra-puzzle` (#533)

---
+++ exercises/practice/zebra-puzzle/src/ZebraPuzzle.elm
@@ -0,0 +1,17 @@
+module ZebraPuzzle exposing (Nationality(..), drinksWater, ownsZebra)
+
+
+type Nationality
+    = Japanese
+    | Norwegian
+    | TODO
+
+
+drinksWater : Maybe Nationality
+drinksWater =
+    Debug.todo "Please implement drinksWater"
+
+
+ownsZebra : Maybe Nationality
+ownsZebra =
+    Debug.todo "Please implement ownsZebra"
