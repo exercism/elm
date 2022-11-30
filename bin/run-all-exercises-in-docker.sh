29c1f71c121cb35e6cb10f394ddcc9f884b46f12
Jeremie Gillet <jie.gillet@gmail.com>
2022-11-30 21:03:36 +0900

    Pull the container rather than building it

--- bin/run-all-exercises-in-docker.sh
+++ bin/run-all-exercises-in-docker.sh
@@ -7,20 +7,17 @@ set -u # Exit on usage of undeclared variable.
 # set -x # Trace what gets executed.
 set -o pipefail # Catch failures in pipes.
 
-# Command line argument
-elm_repo_dir=$1
-
-# build docker image
-docker build --rm -t elm-test-runner .
+# pull docker image
+docker pull exercism/elm-test-runner
 
 # run image passing the arguments
 docker run \
     --rm \
     --read-only \
     --network none \
-    --mount type=bind,src=$(realpath $elm_repo_dir),dst=/opt/test-runner/elm_repo \
+    --mount type=bind,src=${PWD},dst=/opt/test-runner/elm_repo \
     --mount type=tmpfs,dst=/tmp \
-    --volume "$(realpath $elm_repo_dir)/bin/run_all_exercises.sh:/opt/test-runner/bin/run_all_exercises.sh" \
+    --volume "${PWD}/bin/run_all_exercises.sh:/opt/test-runner/bin/run_all_exercises.sh" \
     --entrypoint /opt/test-runner/bin/run_all_exercises.sh \
     elm-test-runner \
     /opt/test-runner/elm_repo
