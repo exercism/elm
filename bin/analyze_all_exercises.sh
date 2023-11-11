4a2542d835ddce51f61c3fe22894aae27c77f716
Jeremie Gillet <jie.gillet@gmail.com>
2023-11-11 20:58:47 +0900

    docker is a pain in the ass

--- bin/analyze_all_exercises.sh
+++ bin/analyze_all_exercises.sh
@@ -11,26 +11,27 @@ set -o pipefail # Catch failures in pipes.
 elm_repo=$1
 
 # Copy repo exercises into temp folder
-exercices=$(mktemp -d /tmp/exercices_XXXXXXXXXX)
-cp -r "$elm_repo/exercises/concept" "$elm_repo/exercises/practice" $exercices
+exercises=$(mktemp -d /tmp/exercises_XXXXXXXXXX)
+cp -r "$elm_repo/exercises/concept" "$elm_repo/exercises/practice" $exercises
 
-build="$exercices/build"
+build="$exercises/build"
 rm -rf $build
 mkdir -p "$build/src" "$build/tests"
 cp "$elm_repo/template/elm.json" "$build/"
 
 # Copying exercises in one directory
-for example_file in $exercices/concept/**/.meta/*.elm
+for example_file in $exercises/concept/**/.meta/*.elm
 do
+  solution=$(jq --raw-output '.files.solution | .[0]' "$exercise_dir/.meta/config.json")
+  exercise_name=$(echo $solution | sed -r 's/.*\/([a-zA-Z]*)\.elm/\1/')
+  echo $exercise_name
   exercise_dir=$(dirname $(dirname $example_file))
-  # get kebab-case slug and transform it to PascalCase
-  exercise_name=$(basename $exercise_dir | sed -r 's/(^|-)([a-z])/\U\2/g')
   cp $exercise_dir/src/*.elm "$build/src/"
   cp $example_file "$build/src/$exercise_name.elm"
   cat "$exercise_dir/tests/Tests.elm" | sed "s/module Tests/module Tests$exercise_name/" | sed 's/skip <|//g' > "$build/tests/Tests$exercise_name.elm"
 done
 
-for example_file in $exercices/practice/**/.meta/src/*.example.elm
+for example_file in $exercises/practice/**/.meta/src/*.example.elm
 do
   exercise_dir=$(dirname $(dirname $(dirname $example_file)))
   exercise_name=$(basename $example_file .example.elm)
