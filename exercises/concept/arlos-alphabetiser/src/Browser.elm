2f7a8a360f48abdbd68ada0ecbd0c7f3e9bee9e2
Cedd Burge <ceddlyburge@gmail.com>
2024-11-11 20:28:21 +0000

    Prove that a shim for Browser.sandbox can work

---
+++ exercises/concept/arlos-alphabetiser/src/Browser.elm
@@ -0,0 +1,13 @@
+module Browser exposing (sandbox)
+
+import Html exposing (Html)
+
+type alias Sandbox msg model =
+    { init : model
+    , update : msg -> model -> model
+    , view : model -> Html msg
+    }
+
+
+sandbox : Sandbox msg model -> Html msg --Sandbox msg model
+sandbox { init, view } = view init
