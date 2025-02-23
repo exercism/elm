88ec44b3faa1ace4ade308a6931f93a20e6b2b1b
ceddlyburge <ceddlyburge@gmail.com>
2025-02-23 20:30:32 +0000

    elm-format

--- exercises/concept/paulas-palindromes/.meta/Exemplar.elm
+++ exercises/concept/paulas-palindromes/.meta/Exemplar.elm
@@ -1,4 +1,4 @@
-module Main exposing (Msg(..), Model, init, main, update, view)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
 import Html exposing (Html, div, input, text)
