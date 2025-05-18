3ea77d9d0eebd0dc697c05a0cc63a45d08e365dc
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-05-18 12:30:11 -0700

    Add `relative-distance`

---
+++ exercises/practice/relative-distance/src/RelativeDistance.elm
@@ -0,0 +1,8 @@
+module RelativeDistance exposing (degreeOfSeparation)
+
+import Dict exposing (Dict)
+
+
+degreeOfSeparation : Dict String (List String) -> String -> String -> Int
+degreeOfSeparation familyTree personA personB =
+    Debug.todo "implement this function"
